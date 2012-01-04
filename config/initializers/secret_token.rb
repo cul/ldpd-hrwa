2ffdf718bcf9555c6d16de816f3a1b3d7eedaee6
James Stuart <github@jamesstuart.org>
2012-01-04 13:42:30 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hrwa::Application.config.secret_token = '4383b64731b65bc681136c95f464c476e592f41592653b6a39b56d416fcd7408d311827d532c70975ae0410883e8977cbef1ec869e4581828aafe9bfb6b961aa'
