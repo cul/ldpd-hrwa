d9625fec11f8fb5e1e4ccb6eaa76fe27efa55edd
David Arjanik <da217@columbia.edu>
2012-01-30 17:27:35 -0500

    static_spec.rb: uncommented page.should

--- spec/requests/static_spec.rb
+++ spec/requests/static_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'home page' do
   it 'has heading' do
     visit '/'
-    #page.should have_content('Hello Home Pagex!')
+    page.should have_content('Hello Home Pagex!')
     true
   end
 end
