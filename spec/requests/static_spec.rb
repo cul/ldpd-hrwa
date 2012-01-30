d6cf9ebf7e585336616469926c1a8f7afa0b6c77
David Arjanik <da217@columbia.edu>
2012-01-30 17:53:50 -0500

    static_spec.rb: all static page routes pass

--- spec/requests/static_spec.rb
+++ spec/requests/static_spec.rb
@@ -6,3 +6,31 @@ describe 'home page' do
     page.should have_content('Hello Home Page!')
   end
 end
+
+describe 'about page' do
+  it 'loads with the correct text' do
+    visit '/about'
+    page.should have_content('Hello About Page!')
+  end
+end
+
+describe 'contact page' do
+  it 'loads with the correct text' do
+    visit '/contact'
+    page.should have_content('Hello Contact Page!')
+  end
+end
+
+describe 'collection browse page' do
+  it 'loads with the correct text' do
+    visit '/browse'
+    page.should have_content('Hello Collection Browse Page!')
+  end
+end
+
+describe 'faq page' do
+  it 'loads with the correct text' do
+    visit '/faq'
+    page.should have_content('Hello FAQ Page!')
+  end
+end
