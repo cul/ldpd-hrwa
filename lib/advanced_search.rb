907ef2d6bd1b7411f616497d5f055591a503f056
David Arjanik <da217@columbia.edu>
2012-02-02 15:12:39 -0500

    advanced_search.rb: implemented process_q_and with passing test
    rspec file uses MockCatalogController

--- lib/advanced_search.rb
+++ lib/advanced_search.rb
@@ -1,30 +1,35 @@
 # Mix in advanced search processing
-module Hrwa::SolrHelper::AdvancedSearch
-  def self.included base
-    # Add q processing
-    base.solr_search_params_logic << [ :process_q_and,
-                                       :process_q_or,
-                                       :process_q_phrase,
-                                       :process_q_exclude, ]
+module AdvancedSearch
+  extend ActiveSupport::Concern
+ 
+  included do 
+    # Build q param
+    self.solr_search_params_logic << :process_q_and
+    self.solr_search_params_logic << :process_q_or
+    self.solr_search_params_logic << :process_q_phrase
+    self.solr_search_params_logic << :process_q_exclude    
   end  
   
-  # solr_search_params_logic methods take two arguments
-  # @param [Hash] solr_parameters a hash of parameters to be sent to Solr (via RSolr)
-  # @param [Hash] user_parameters a hash of user-supplied parameters (often via `params`)  
-  def process_q_and solr_parameters, user_parameters
+  module ClassMethods
+    # solr_search_params_logic methods take two arguments
+    # @param [Hash] solr_parameters a hash of parameters to be sent to Solr (via RSolr)
+    # @param [Hash] user_parameters a hash of user-supplied parameters (often via `params`) 
+     
+    def process_q_and solr_parameters, user_parameters
+      solr_parameters[ :q ] << " +women's +rights +africa"
+    end
     
-  end
-  
-  def process_q_or solr_parameters, user_parameters
+    def process_q_or solr_parameters, user_parameters
+      
+    end
     
-  end
-  
-  def process_q_phrase solr_parameters, user_parameters
+    def process_q_phrase solr_parameters, user_parameters
+      
+    end
     
+    def process_q_exclude solr_parameters, user_parameters
+      
+    end
   end
-  
-  def process_q_exclude solr_parameters, user_parameters
-    
-  end
-  
 end
+ 
---
+++ spec/lib/advanced_search_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+require 'advanced_search'
+
+class MockCatalogController
+  def self.solr_search_params_logic 
+    return [ ]
+  end
+  
+  include AdvancedSearch 
+end
+
+describe 'process_q_and' do
+  it 'prepends plus sign before each term' do
+    solr_parameters = { :q => '' }
+    user_parameters = { :q_and => "women's rights africa" }
+    
+    MockCatalogController::process_q_and solr_parameters, user_parameters
+    
+    solr_parameters[ :q ].should eq " +women's +rights +africa"
+  end
+end
+
+# describe 'process_q_or' do
+  # it '?' do
+#     
+  # end
+# end
+# 
+# describe 'process_q_phrase' do
+  # it 'wraps whole textbox input in double quotes' do
+#     
+  # end
+# end
+# 
+# describe 'process_q_exclude' do
+  # it 'prepends minus sign before each term' do
+#     
+  # end
+# end
