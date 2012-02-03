b3e454a431d7b2056a7f9b7de1e632aa02697af5
David Arjanik <da217@columbia.edu>
2012-02-03 11:21:48 -0500

    CatalogController: params[ :mode ] if block

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -6,7 +6,6 @@ class CatalogController < ApplicationController
   before_filter :_configure_by_search_type
 
   include Blacklight::Catalog
-  include AdvancedSearch
 
   private
 
@@ -16,11 +15,14 @@ class CatalogController < ApplicationController
 
     @configurator = Configurator.new( @search_type )
     
-    self.solr_search_params_logic << :process_q_and
-    self.solr_search_params_logic << :process_q_or
-    self.solr_search_params_logic << :process_q_phrase
-    self.solr_search_params_logic << :process_q_exclude  
-    
+    if params[ :mode ] == :advanced
+      include AdvancedSearch
+      self.solr_search_params_logic << :process_q_and
+      self.solr_search_params_logic << :process_q_or
+      self.solr_search_params_logic << :process_q_phrase
+      self.solr_search_params_logic << :process_q_exclude  
+    end
+   
     # CatalogController.configure_blacklight yields a Blacklight::Configuration object
     # that expects a block/proc which sets its attributes accordingly
     CatalogController.configure_blacklight( &@configurator.config_proc )
