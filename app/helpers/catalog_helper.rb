b9183f50de688b2ce75bc91e437896fff7e01c78
David Arjanik <da217@columbia.edu>
2012-02-07 18:51:07 -0500

    created catalog_helper_behavior.rb: contains local overrides of Blacklight module

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -1,3 +1,22 @@
+require 'Hrwa/catalog_helper_behavior'
+
 module CatalogHelper
   include Blacklight::CatalogHelperBehavior
+  
+  # Contains local overrides of Blacklight::CatalogHelperBehavior methods
+  include Hrwa::CatalogHelperBehavior
+  
+  # Override
+  def has_search_parameters?
+    if params[ :search_mode ] == 'advanced' 
+      return( ! params[ :q_and        ].blank? or 
+              ! params[ :q_exclude    ].blank? or
+              ! params[ :q_phrase     ].blank? or
+              ! params[ :q_not        ].blank? or
+              ! params[ :f            ].blank? or
+              super )
+    else
+      super
+    end
+  end  
 end
