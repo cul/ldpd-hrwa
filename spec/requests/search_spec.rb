f495bee41512fe1ca4c3d8a54688798bf50677a7
David Arjanik <da217@columbia.edu>
2012-02-08 11:58:44 -0500

    search_spec.rb: re-enabled a failing test that was commented out

--- spec/requests/search_spec.rb
+++ spec/requests/search_spec.rb
@@ -4,10 +4,10 @@ describe 'advanced_search' do
   # Advanced search doesn't use 'q' param, in such case Blacklight often by default sends user to Home view
   # We are overriding so that if 'q' is absent but 'q_*' param(s) are present, the index view still
   # gets rendered
-  # it 'informs user "No results found" if advanced search returns no hits' do
-    # visit '/search?type=archive&search_mode=advanced&q_and=and1+and2+and3'
-    # page.should have_content('No results found')
-  # end
+  it 'informs user "No results found" if advanced search returns no hits' do
+    visit '/search?type=archive&search_mode=advanced&q_and=and1+and2+and3'
+    page.should have_content('No results found')
+  end
   
   # This should never really happen, but just in case...
   it 'renders the search page if there are no q_* or limit by params (if the are present' +
