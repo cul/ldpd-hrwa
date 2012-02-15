c183a6e8243c81e8d3d86a47f3e505e68e6b6c90
David Arjanik <da217@columbia.edu>
2012-02-14 19:58:39 -0500

    rm'ed public/*_solr_response.txt files.  They are in wiki now.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -32,12 +32,13 @@ class CatalogController < ApplicationController
     end
 
     # TODO: remove me
-    if(@search_type == :archive)
-      render :text => %Q{CatalogController currently broken.  This is a temporary
-                      manual render to keep Rails from crashing.\n
-                      <br/>"Search Tips" - this string is here to enable Capybara request test to pass} \
-                      and return
-    end
+    debugger
+    # if(@search_type == :archive)
+      # render :text => %Q{CatalogController currently broken.  This is a temporary
+                      # manual render to keep Rails from crashing.\n
+                      # <br/>"Search Tips" - this string is here to enable Capybara request test to pass} \
+                      # and return
+    # end
 
     (@response, @document_list) = @get_search_results_method.call( params,
                                                                   extra_controller_params ||= {} )
