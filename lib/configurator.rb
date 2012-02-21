17dec43ce368b2ad48bb49f0bf27ce7fdcb02804
David Arjanik <da217@columbia.edu>
2012-02-21 11:45:40 -0500

    Configurator: NEW methods post_blacklight_processing_required? and post_blacklight_processing\!

--- lib/configurator.rb
+++ lib/configurator.rb
@@ -17,6 +17,18 @@ class Configurator
   def config_proc
     return @configurator.config_proc
   end
+  
+  # Did Blacklight give us everything we need in SOLR response and
+  # results list objects?
+  def post_blackight_processing_required?
+    return @configurator.post_blacklight_processing_required?
+  end
+  
+  # Do more with the SOLR response and results list that Blacklight
+  # gives us.
+  def post_blacklight_processing!( solr_response, results_list )
+    return @configurator.post_blacklight_processing!( solr_response, results_list )
+  end
 
   def result_partial
     return @configurator.result_partial
