31102d4619a4566932bceeb106c9fc460f758071
David Arjanik <da217@columbia.edu>
2012-02-23 14:30:53 -0500

    catalog_helper: removed duplicate has_search_parameters? def

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -3,18 +3,4 @@ module CatalogHelper
 
   # Contains local overrides of Blacklight::CatalogHelperBehavior methods
   include HRWA::CatalogHelperBehavior
-
-  # Override
-  def has_search_parameters?
-    if params[ :search_mode ] == 'advanced'
-      return( ! params[ :q_and        ].blank? or
-              ! params[ :q_exclude    ].blank? or
-              ! params[ :q_phrase     ].blank? or
-              ! params[ :q_not        ].blank? or
-              ! params[ :f            ].blank? or
-              super )
-    else
-      super
-    end
-  end
 end
