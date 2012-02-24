14877e472e37f08a4efb498f79fa17a88898f317
David Arjanik <da217@columbia.edu>
2012-02-24 14:57:25 -0500

    catalog_helper_behavior_spec: improved see_all_hits_from_domain_link

--- spec/helpers/catalog_helper_behavior_spec.rb
+++ spec/helpers/catalog_helper_behavior_spec.rb
@@ -5,7 +5,7 @@ include HRWA::CatalogHelperBehavior
 
 describe 'see_all_hits_from_domain_link' do
   it 'creates correct link tag for domain:www.hrwa.org' do
-    params_unsorted = { :search_type => 'archive', :search_mode => 'advanced', :q_and => 'women', :q_phrase => '', :q_or => '', :q_exclude => '', :lim_domain => '', :lim_mimetype => '', :lim_language => '', :lim_geographic_focus => '', :lim_organization_based_in => '', :lim_organization_type => '', :lim_creator_name => '', :crawl_start_date => '', :crawl_end_date => '', :rows => '10', :sort => 'score+desc', :solr_host => 'harding.cul.columbia.edu', :solr_core_path => '%2Fsolr-4%2Fasf', :submit_search => 'Advanced+Search' }
+    params_unsorted = { :search_type => 'archive', :search_mode => 'advanced', :q => 'women', :q_and => 'women', :q_phrase => '', :q_or => '', :q_exclude => '', :lim_domain => '', :lim_mimetype => '', :lim_language => '', :lim_geographic_focus => '', :lim_organization_based_in => '', :lim_organization_type => '', :lim_creator_name => '', :crawl_start_date => '', :crawl_end_date => '', :rows => '10', :sort => 'score+desc', :solr_host => 'harding.cul.columbia.edu', :solr_core_path => '%2Fsolr-4%2Fasf', :submit_search => 'Advanced+Search' }
     params_expected_in_url = params_unsorted.merge( :'f[domain][]' => 'www.hrw.org' )
     
     # The method being tested uses sorted params
@@ -19,10 +19,6 @@ describe 'see_all_hits_from_domain_link' do
     expected_link_tag << %Q{">See all hits from &quot;www.hrw.org&quot;</a>}
     
     link_tag = see_all_hits_from_domain_link( params_unsorted, 'www.hrw.org' )
-    
-    expected_chars = expected_link_tag.split('')
-    link_chars     = link_tag.split('')
-    
     link_tag.should == expected_link_tag
   end
 end
