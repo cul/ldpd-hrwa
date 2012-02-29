f87cdc3957fb949c27c23843973e8ee9ddbe3fde
David Arjanik <da217@columbia.edu>
2012-02-29 11:56:35 -0500

    AdvancedSearch::LimitBy - replaced old stuff with #limit_by_user_params and #add_limit_by_filter_queries_to_solr

--- lib/hrwa/advanced_search/limit_by.rb
+++ lib/hrwa/advanced_search/limit_by.rb
@@ -1,8 +1,11 @@
 # Mix in advanced search limit-by processing
 module HRWA::AdvancedSearch::LimitBy
-  def add_limit_by_domain_to_fq( solr_parameters, user_parameters  )
-    solr_parameters[ :fq ] ||= []
-    domain = user_parameters[ :lim_domain ]
-    solr_parameters[ :fq ] << "domain:#{ domain }"
+  
+  def limit_by_user_params( user_parameters = params )
+    return user_parameters.select { | param, value | param =~ /^lim_.*/ && ! value.empty? }
+  end
+  
+  def add_limit_by_filter_queries_to_solr( solr_parameters, user_parameters = params )
+    
   end
 end
--- spec/lib/hrwa/advanced_search/limit_by_spec.rb
+++ spec/lib/hrwa/advanced_search/limit_by_spec.rb
@@ -6,19 +6,34 @@ end
 
 mock_catalog_controller = MockCatalogController.new
 
-  describe 'HRWA::AdvancedSearch::LimitBy' do
-    before( :each ) do
-      @user_parameters = { :search_type => 'archive', :search_mode => 'advanced', :q_and => '', :q_phrase => '', :q_or => '', :q_exclude => '', :lim_domain => '', :lim_mimetype => '', :lim_language => '', :lim_geographic_focus => '', :lim_organization_based_in => '', :lim_organization_type => '', :lim_creator_name => '', :crawl_start_date => '', :crawl_end_date => '', :rows => '10', :sort => 'score+desc', :solr_host => 'harding.cul.columbia.edu', :solr_core_path => '%2Fsolr-4%2Fasf', :submit_search => 'Advanced+Search', }
-      @solr_parameters = {  }
-    end
-   
-    describe '#add_limit_by_domain adds one domain filter query correctly' do
-     
-      it 'creates filter query correctly for domain limit-by' do
-        @user_parameters[ :lim_domain ] = 'advocacyforum.org'
-        mock_catalog_controller.add_limit_by_domain_to_fq @solr_parameters, @user_parameters
-        @solr_parameters[ :fq ].should == [ 'domain:advocacyforum.org' ]
-      end
-     
-    end
+describe 'HRWA::AdvancedSearch::LimitBy' do
+  before( :each ) do
+    @user_parameters = { :search_type => 'archive', :search_mode => 'advanced', :q_and => '', :q_phrase => '', :q_or => '', :q_exclude => '', :lim_domain => '', :lim_mimetype => '', :lim_language => '', :lim_geographic_focus => '', :lim_organization_based_in => '', :lim_organization_type => '', :lim_creator_name => '', :crawl_start_date => '', :crawl_end_date => '', :rows => '10', :sort => 'score+desc', :solr_host => 'harding.cul.columbia.edu', :solr_core_path => '%2Fsolr-4%2Fasf', :submit_search => 'Advanced+Search', }
+    @solr_parameters = {  }
   end
+  
+  describe '#limit_by_user_params' do
+
+    it 'returns empty hash if no lim_* user params present' do
+      mock_catalog_controller.limit_by_user_params( @user_parameters ).should == {}
+    end
+    
+    it 'returns hash of lim_* user params if present' do
+      lim_params = { :lim_domain => 'advocacyforum.org', :lim_language => 'English' }
+      @user_parameters.merge!( lim_params )
+      mock_catalog_controller.limit_by_user_params( @user_parameters ).should == lim_params
+    end
+
+  end
+ 
+  # describe '#add_limit_by_filter_queries_to_solr' do
+# 
+    # it 'creates filter query correctly for domain limit-by' do
+      # @user_parameters[ :lim_domain ] = 'advocacyforum.org'
+      # mock_catalog_controller.add_limit_by_filter_queries_to_solr @solr_parameters, @user_parameters
+      # @solr_parameters[ :fq ].should == [ 'domain:advocacyforum.org' ]
+    # end
+#    
+  # end
+  
+end
