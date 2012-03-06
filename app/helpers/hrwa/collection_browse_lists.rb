01ea3dd50758a2f3e286977c67c9e4d0512a38a1
David Arjanik <da217@columbia.edu>
2012-03-05 20:46:22 -0500

    HRWA::CollectionBrowseLists implemented with tests and hardcoded lists that are updated by a separate script

---
+++ app/helpers/hrwa/collection_browse_lists.rb
@@ -0,0 +1,52 @@
+# -*- encoding : utf-8 -*-
+module HRWA::CollectionBrowseLists
+  include HRWA::CollectionBrowseListsSourceHardcoded
+  
+  def browse_list( browse_category_name )
+    
+    begin
+      browse_list_hash = self.send( browse_category_name + '_browse_list_items' )
+    rescue NoMethodError
+      raise ArgumentError, "No such browse category as #{ browse_category_name }"  
+    end
+    
+    return browse_list_hash
+  end
+  
+  def creator_name_browse_list
+    return browse_list( 'creator_name' )
+  end
+
+  def domain_browse_list
+    return browse_list( 'domain' )
+  end
+  
+  def geographic_focus_browse_list
+    return browse_list( 'geographic_focus' )
+  end
+  
+  def language_browse_list
+    return browse_list( 'language' )
+  end
+  
+  def organization_based_in_browse_list
+    return browse_list( 'organization_based_in' )
+  end
+  
+  def organization_type_browse_list
+    return browse_list( 'organization_type' )
+  end
+  
+  def original_urls_browse_list
+    return browse_list( 'original_urls' )
+  end
+  
+  def subject_browse_list
+    return browse_list( 'subject' )
+  end
+
+  def title_browse_list
+    return browse_list( 'title' )
+  end
+
+end
