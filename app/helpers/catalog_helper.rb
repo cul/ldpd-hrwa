33be13cf4dd774782a7293f9ae478bdde6d4eed4
David Arjanik <da217@columbia.edu>
2012-03-06 17:55:56 -0500

    CatalogHelper: temporarily restored include for BrowseOptions to keep website from breaking

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -4,6 +4,7 @@ module CatalogHelper
   # Contains local overrides of Blacklight::CatalogHelperBehavior methods
   include HRWA::CatalogHelperBehavior
 
+  include HRWA::BrowseOptions
   include HRWA::CollectionBrowseLists
   include HRWA::FilterOptions
   
