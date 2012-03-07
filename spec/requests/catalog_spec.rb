23a4b9d6e3a808cf38151e1074b4e5c6b62898e1
David Arjanik <da217@columbia.edu>
2012-03-06 20:50:24 -0500

    requests/catalog_spec.rb: re-ordered example groups

--- spec/requests/catalog_spec.rb
+++ spec/requests/catalog_spec.rb
@@ -17,68 +17,43 @@ describe 'all searches' do
   end
 end
 
-describe 'advanced_search_asf' do
-  it 'informs user "No results found" if advanced search returns no hits' do
-    visit '/advanced_asf'
-    fill_in 'q_and', :with => 'zzzzzzzzzzzzzzzzzzaaaaaaaaaaaaaaaa'
-    click_button 'submit_search'
-    page.should have_content('No results found')
+# # TODO: change this to form fill-in
+describe 'archive search' do
+  it 'does not raise an error when paging through results' do
+    visit '/catalog?page=3&q=water&search_type=archive&search=true'
+    page.should_not have_content( %q{can't convert Fixnum into String} )
   end
 
-  describe 'q_and=women search' do
-    before :each do
-      visit '/advanced_asf'
-      fill_in 'q_and', :with => 'women'
-      click_button 'submit_search'
-      @result_page = page
-    end
-
-    it 'informs user "Click on + to refine search" in simple search box if doing advanced search' do
-      @result_page.has_field?( 'q', :with => '[Click + to refine search]' ).should == true
-    end
-
-    it 'creates the correct HTTP querystring for simple "q_and" search' do
-      querystring = URI.parse( current_url ).query
-      querystring.should == "search_type=archive&search_mode=advanced&search=true&q_and=women&q_phrase=&q_or=&q_exclude=&lim_domain=&lim_mimetype=&lim_language=&lim_geographic_focus=&lim_organization_based_in=&lim_organization_type=&lim_creator_name=&crawl_start_date=&crawl_end_date=&rows=10&sort=score+desc&solr_host=harding.cul.columbia.edu&solr_core_path=%2Fsolr-4%2Fasf&submit_search=Advanced+Search"
-    end
-
-    # TODO: These are just some cheap, temporary tests for assistance during initial development.
-    # SOLR index is stable but later will be incrementally updated so these tests are super-brittle.
-    # Delete them when Rails Port milestone is reached.
-    it 'returns 3,262,426 results for q_and=women (TEMPORARY TEST: DELETE ME LATER)' do
-      @result_page.should have_content( '3,262,426' )
-    end
-
+  describe 'facets' do
+    configurator        = HRWA::ArchiveSearchConfigurator.new
+    blacklight_config   = Blacklight::Configuration.new
+    config_proc         = configurator.config_proc
+    blacklight_config.configure &config_proc
+ 
+     blacklight_config.facet_fields.each { | facet_name, facet_field |
+      it "has working pagination for #{ facet_name } facet" do  
+        visit "/catalog/facet/#{ facet_name }?q=rights&search=true&search_type=archive&utf8=%E2%9C%93"
+        page.should have_content( facet_field.label )
+        page.status_code.should == 200
+      end
+    }
   end
-
 end
 
-describe 'advanced_search_fsf' do
-
-  it 'informs user "No results found" if advanced search returns no hits' do
-    visit '/advanced_fsf'
-    fill_in 'q_and', :with => 'zzzzzzzzzzzzzzzzzzaaaaaaaaaaaaaaaa'
-    click_button 'submit_search'
-    page.should have_content('No results found')
-  end
-
-  it 'informs user "Click on + to refine search" in simple search box if doing advanced search' do
-    visit '/advanced_fsf'
-    fill_in 'q_and', :with => 'water'
-    click_button 'submit_search'
-    page.has_field?( 'q', :with => '[Click + to refine search]' ).should == true
-  end
-
-  it 'returns search results for a known successful query' do
-    visit '/advanced_fsf'
-    fill_in 'q_and', :with => 'water'
-    fill_in 'q_phrase', :with => 'Provides information'
-    fill_in 'q_or', :with => 'human rights'
-    fill_in 'q_exclude', :with => 'zamboni'
-    click_button 'submit_search'
-    page.should have_content('Center for Economic and Social Rights')
-  end
-
+# TODO: change this to form fill-in
+describe 'find site search' do
+  configurator        = HRWA::FindSiteSearchConfigurator.new
+  blacklight_config   = Blacklight::Configuration.new
+  config_proc         = configurator.config_proc
+  blacklight_config.configure &config_proc
+     
+  blacklight_config.facet_fields.each { | facet_name, facet_field |
+    it "has working pagination for #{ facet_name } facet" do  
+      visit "/catalog/facet/#{ facet_name }?q=rights&search=true&search_type=find_site&utf8=%E2%9C%93"
+      page.should have_content( facet_field.label )
+      page.status_code.should == 200
+    end
+  }
 end
 
 # JIRA issue: https://issues.cul.columbia.edu/browse/HRWA-324
@@ -161,41 +136,67 @@ end
 #
 # end
 
-# # TODO: change this to form fill-in
-describe 'archive search' do
-  it 'does not raise an error when paging through results' do
-    visit '/catalog?page=3&q=water&search_type=archive&search=true'
-    page.should_not have_content( %q{can't convert Fixnum into String} )
+# TODO: convert these to production search form tests
+describe 'advanced_search_asf' do
+  it 'informs user "No results found" if advanced search returns no hits' do
+    visit '/advanced_asf'
+    fill_in 'q_and', :with => 'zzzzzzzzzzzzzzzzzzaaaaaaaaaaaaaaaa'
+    click_button 'submit_search'
+    page.should have_content('No results found')
   end
 
-  describe 'facets' do
-    configurator        = HRWA::ArchiveSearchConfigurator.new
-    blacklight_config   = Blacklight::Configuration.new
-    config_proc         = configurator.config_proc
-    blacklight_config.configure &config_proc
- 
-     blacklight_config.facet_fields.each { | facet_name, facet_field |
-      it "has working pagination for #{ facet_name } facet" do  
-        visit "/catalog/facet/#{ facet_name }?q=rights&search=true&search_type=archive&utf8=%E2%9C%93"
-        page.should have_content( facet_field.label )
-        page.status_code.should == 200
-      end
-    }
+  describe 'q_and=women search' do
+    before :each do
+      visit '/advanced_asf'
+      fill_in 'q_and', :with => 'women'
+      click_button 'submit_search'
+      @result_page = page
+    end
+
+    it 'informs user "Click on + to refine search" in simple search box if doing advanced search' do
+      @result_page.has_field?( 'q', :with => '[Click + to refine search]' ).should == true
+    end
+
+    it 'creates the correct HTTP querystring for simple "q_and" search' do
+      querystring = URI.parse( current_url ).query
+      querystring.should == "search_type=archive&search_mode=advanced&search=true&q_and=women&q_phrase=&q_or=&q_exclude=&lim_domain=&lim_mimetype=&lim_language=&lim_geographic_focus=&lim_organization_based_in=&lim_organization_type=&lim_creator_name=&crawl_start_date=&crawl_end_date=&rows=10&sort=score+desc&solr_host=harding.cul.columbia.edu&solr_core_path=%2Fsolr-4%2Fasf&submit_search=Advanced+Search"
+    end
+
+    # TODO: These are just some cheap, temporary tests for assistance during initial development.
+    # SOLR index is stable but later will be incrementally updated so these tests are super-brittle.
+    # Delete them when Rails Port milestone is reached.
+    it 'returns 3,262,426 results for q_and=women (TEMPORARY TEST: DELETE ME LATER)' do
+      @result_page.should have_content( '3,262,426' )
+    end
+
   end
+
 end
 
-# TODO: change this to form fill-in
-describe 'find site search' do
-  configurator        = HRWA::FindSiteSearchConfigurator.new
-  blacklight_config  = Blacklight::Configuration.new
-  config_proc         = configurator.config_proc
-  blacklight_config.configure &config_proc
-     
-  blacklight_config.facet_fields.each { | facet_name, facet_field |
-    it "has working pagination for #{ facet_name } facet" do  
-      visit "/catalog/facet/#{ facet_name }?q=rights&search=true&search_type=find_site&utf8=%E2%9C%93"
-      page.should have_content( facet_field.label )
-      page.status_code.should == 200
-    end
-  }
+describe 'advanced_search_fsf' do
+
+  it 'informs user "No results found" if advanced search returns no hits' do
+    visit '/advanced_fsf'
+    fill_in 'q_and', :with => 'zzzzzzzzzzzzzzzzzzaaaaaaaaaaaaaaaa'
+    click_button 'submit_search'
+    page.should have_content('No results found')
+  end
+
+  it 'informs user "Click on + to refine search" in simple search box if doing advanced search' do
+    visit '/advanced_fsf'
+    fill_in 'q_and', :with => 'water'
+    click_button 'submit_search'
+    page.has_field?( 'q', :with => '[Click + to refine search]' ).should == true
+  end
+
+  it 'returns search results for a known successful query' do
+    visit '/advanced_fsf'
+    fill_in 'q_and', :with => 'water'
+    fill_in 'q_phrase', :with => 'Provides information'
+    fill_in 'q_or', :with => 'human rights'
+    fill_in 'q_exclude', :with => 'zamboni'
+    click_button 'submit_search'
+    page.should have_content('Center for Economic and Social Rights')
+  end
+
 end
