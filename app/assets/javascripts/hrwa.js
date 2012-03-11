8ca1a279337821e6ec68604a456b008455d0cc7e
erik r <er2576@columbia.edu>
2012-03-11 12:53:24 -0400

    roundtrip redux 2

--- app/assets/javascripts/hrwa.js
+++ app/assets/javascripts/hrwa.js
@@ -369,10 +369,10 @@ $( ".datepicker" ).datepicker({
 // reverse parse advanced form inputs to q
 function adv_to_q() {
  if ($('#q').val() == '') {
-  var erq_and = $('#q_and').val().replace(/^|\s(?=[^ ])/g, ' +');
-  var erq_exclude = $('#q_exclude').val().replace(/^|\s(?=[^ ])/g, ' -');
-  var erq_or = $('#q_or').val().replace(/^|\s/g, ' ');
-  var erq_phrase = '"'+$('#q_phrase').val()+'"';
+  var erq_and = ($('#q_and').val() != '') ? $('#q_and').val().replace(/^|\s(?=[^ ])/g, ' +') : '';
+  var erq_exclude = ($('#q_exclude').val() != '') ? $('#q_exclude').val().replace(/^|\s(?=[^ ])/g, ' -') : '';
+  var erq_or = ($('#q_or').val() != '') ? $('#q_or').val().replace(/^|\s/g, ' ') : '';
+  var erq_phrase = ($('#q_phrase').val() != '') ? '"'+$('#q_phrase').val()+'"' : '';
   $('#q').val(erq_and + ' ' + erq_exclude + ' ' + erq_phrase + ' ' + erq_or)
  }
 }
@@ -383,15 +383,15 @@ function q_to_adv() {
   $('#q_phrase').val(erq_to_q_phrase);
  }
  if($('#q_and').val() == '') {
-  var erq_to_q_and = ( $('#q').val().match(/\+[A-Za-z0-9]+\s/g) ) ? $('#q').val().match(/\+[A-Za-z0-9]+\s/g).join('').replace(/\+/g,'') : '';
+  var erq_to_q_and = ( $('#q').val().match(/\+[A-Za-z0-9]+\s?/g) ) ? $('#q').val().match(/\+[A-Za-z0-9]+\s?/g).join('').replace(/\+/g,'') : '';
   $('#q_and').val(erq_to_q_and);
  }
  if($('#q_exclude').val() == '') {
-  var erq_to_q_exclude = ( $('#q').val().match(/\-[A-Za-z0-9]+\s/g) ) ? $('#q').val().match(/\-[A-Za-z0-9]+\s/g).join('').replace(/\-/g,'') : '';
+  var erq_to_q_exclude = ( $('#q').val().match(/\-[A-Za-z0-9]+\s?/g) ) ? $('#q').val().match(/\-[A-Za-z0-9]+\s?/g).join('').replace(/\-/g,'') : '';
   $('#q_exclude').val(erq_to_q_exclude);
  }
  if($('#q_or').val() == '') {
-  var erq_to_q_or = ( $('#q').val().match(/\s[A-Za-z0-9]+(?=([^"]*"[^"]*")*[^"]*$)/g) ) ? $('#q').val().match(/\s[A-Za-z0-9]+(?=([^"]*"[^"]*")*[^"]*$)/g).join('') : '';
+  var erq_to_q_or = ( $('#q').val().match(/(^|\s)+[A-Za-z0-9]+(?=([^"]*"[^"]*")*[^"]*$)/g) ) ? $('#q').val().match(/(^|\s)+[A-Za-z0-9]+(?=([^"]*"[^"]*")*[^"]*$)/g).join('') : '';
   $('#q_or').val(erq_to_q_or);
  }
 }
