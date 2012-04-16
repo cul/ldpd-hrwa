e7779e268e8397cdc7b619738a25c3e0b76eb00d
erik r <er2576@columbia.edu>
2012-04-16 14:50:29 -0400

    public_bugreport and public_feedback forms.

--- app/assets/javascripts/hrwa.js
+++ app/assets/javascripts/hrwa.js
@@ -912,8 +912,16 @@ jQuery(function($) {
   $('#frmWebsiteNomination').attr('action', 'https://www1.columbia.edu/sec-cgi-bin/cul/ldpd/prod/humanrights/hrwa/ownernomination');
   $('#frmWebsiteNomination-pub').attr('action', 'https://www1.columbia.edu/sec-cgi-bin/cul/ldpd/prod/humanrights/hrwa/publicnomination');
 
+  $('#frmPublicFeedback').attr('action', 'http://www.columbia.edu/cgi-bin/cul/ldpd-inbox.pl');
+
 // email mailto: to slow robots.
   var culhrwebmail = 'culhrweb'+'@'+'libraries.cul.columbia.'+'edu'
-  $('.culhrweb-email').attr('href', 'm'+'ailto:'+culhrwebmail).html(culhrwebmail);
+  var culhrweb-bugreports = 'culhrweb-bugreports'+'@'+'libraries.cul.columbia.'+'edu'
+  var culhrweb-all = 'culhrweb-all'+'@'+'libraries.cul.columbia.'+'edu'
+  $('a.culhrweb-email').attr('href', 'm'+'ailto:'+culhrwebmail).html(culhrwebmail);
+  $('a.culhrweb-bugreports').attr('href', 'm'+'ailto:'+culhrwebmail).html(culhrwebmail);
+  $('a.culhrweb-all').attr('href', 'm'+'ailto:'+culhrwebmail).html(culhrwebmail);
+  $('input.culhrweb-all').attr('value', 'culhrweb-all'+'@'+'libraries.cul.columbia.'+'edu');
+  $('input.culhrweb-bugreports').attr('value', 'culhrweb-bugreports'+'@'+'libraries.cul.columbia.'+'edu');
 
 }); // ready
