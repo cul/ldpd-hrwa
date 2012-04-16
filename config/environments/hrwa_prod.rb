037e802577397c2decf3e85a7629a0cba17eda18
David Arjanik <da217@columbia.edu>
2012-04-16 17:04:05 -0400

    hrwa_prod.rb: set config.assets.compile = false as per Jame's recommendation.  See HRWA-380

--- config/environments/hrwa_prod.rb
+++ config/environments/hrwa_prod.rb
@@ -24,7 +24,7 @@ config.action_mailer.smtp_settings = {
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
   config.assets.compress = true
-  config.assets.compile = true
+  config.assets.compile = false
   config.assets.digest = true
 
   # Only use best-standards-support built into browsers
