08078f36f09eecd24141b648faf1ea196e3a8944
Eric O <elo2112@columbia.edu>
2012-04-17 14:10:33 -0400

    Added handy debug info to admin index page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,11 +6,7 @@ class AdminController < ApplicationController
   before_filter :authenticate_admin!
 
   def index
-		if admin_signed_in?
-			@admin_is_signed_in = true;
-		else
-			@admin_is_signed_in = false;
-		end
+		
   end
 
 end
