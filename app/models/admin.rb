b641af37f6f8d1ae6d3e3d5f2cfa4f563e85c576
Eric O <elo2112@columbia.edu>
2012-04-17 13:37:48 -0400

    Logins are now username based

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,9 +2,16 @@ class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise    :database_authenticatable,
-            #:registerable,
+            :registerable,
             :recoverable, :rememberable, :trackable, :validatable, :timeoutable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+
+  # Add custom validation for username - This is necessary
+  # because while email is validated by default, username
+  # is not (even though it's unique in the database).
+  validates_presence_of :username
+  validates_uniqueness_of :username
+
 end
