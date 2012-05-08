ba8fc94edf4471d8b1193ca079e78d77935ca6de
David Arjanik <da217@columbia.edu>
2012-05-07 20:42:12 -0400

    config/deploy.rb: reinstated new tag format from 666066820d390b4556f825c388370570fb9a80a8

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,10 @@ set :use_sudo, false
 namespace :deploy do
   desc "Add tag based on current version"
   task :auto_tag, :roles => :app do
-    current_version = IO.read("VERSION").strip + Date.today.strftime("-%y%m%d")
+    current_version = 'v' + 
+                      IO.read("VERSION").strip + 
+                      "/" +
+                      DateTime.now.strftime("%Y%m%d")
     tag = Capistrano::CLI.ui.ask "Tag to add: [#{current_version}] "
     tag = current_version if tag.empty?
 
@@ -47,7 +50,7 @@ namespace :deploy do
   end
 
 
-  desc "Compile asets"
+  desc "Compile assets"
   task :assets do
     run "cd #{release_path}; RAILS_ENV=#{rails_env} bundle exec rake assets:clean assets:precompile"
   end
