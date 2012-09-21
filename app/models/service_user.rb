2ff8da236e07ed4a78e951b4d9165efc0a8cef16
Eric O <elo2112@columbia.edu>
2012-09-21 12:23:23 -0400

    Disabled service user registration; Added admin link that displays when logged in

--- app/models/service_user.rb
+++ app/models/service_user.rb
@@ -2,8 +2,10 @@ class ServiceUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # HRWA Note: Removed :registerable because we don't want people to register new service users
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :password, :email, :password_confirmation, :remember_me
