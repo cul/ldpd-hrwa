dbb4eb733a8c3ca6bc5e373e313faef6bef97bb0
Eric <eric@ejdigitalmedia.com>
2012-10-23 22:15:29 -0400

    Updated contact notification email addresses

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,14 +13,14 @@ class PagesController < ApplicationController
 
   def owner_nomination
 		if(params[:owner_nomination])
-      send_mail_owner_nomination( 'elo2112@columbia.edu' )
+      send_mail_owner_nomination( 'culhrweb@libraries.cul.columbia.edu' )
       flash.now[:notice] = 'Thank you.  Your nomination has been submitted.'
 		end
   end
 
   def public_nomination
 		if(params[:public_nomination])
-      send_mail_public_nomination( 'elo2112@columbia.edu' )
+      send_mail_public_nomination( 'culhrweb@libraries.cul.columbia.edu' )
       flash.now[:notice] = 'Thank you.  Your nomination has been submitted.'
 		end
   end
@@ -30,14 +30,14 @@ class PagesController < ApplicationController
 
   def contact
 		if(params[:contact])
-      send_mail_contact( 'elo2112@columbia.edu' )
+      send_mail_contact( 'culhrweb@libraries.cul.columbia.edu' )
       flash.now[:notice] = 'Thank you.  Your message has been submitted.'
 		end
   end
 
   def problem_report
 		if(params[:problem_report])
-      send_mail_problem_report( 'elo2112@columbia.edu' )
+      send_mail_problem_report( 'hrwa-developers@libraries.cul.columbia.edu' )
       flash.now[:notice] = 'Thank you.  Your problem report has been submitted.'
 		end
   end
