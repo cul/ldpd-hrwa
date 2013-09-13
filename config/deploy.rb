e1cbffb5284a4e96e9e71894e279806d4d0eece8
Eric O'Hanlon <eric@ejdigitalmedia.com>
2013-09-12 22:48:21 -0400

    Updated deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,6 +46,7 @@ namespace :deploy do
     run "ln -nfs #{deploy_to}shared/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{deploy_to}shared/app_config.yml #{release_path}/config/app_config.yml"
     run "ln -nfs #{deploy_to}shared/fedora.yml #{release_path}/config/fedora.yml"
+    run "ln -nfs #{deploy_to}shared/fedora.yml #{release_path}/config/default_users.yml"
     run "mkdir -p #{release_path}/db"
     run "ln -nfs #{deploy_to}shared/#{rails_env}.sqlite3 #{release_path}/db/#{rails_env}.sqlite3"
   end
