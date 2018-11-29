ced386cd2cc6edb64c6b3fba24b795f560548cc7
JackBlackLight <jd2148@columbia.edu>
2018-11-29 14:09:03 -0500

    adds first version of thumbnail helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
+require 'net/http'
+
 module ApplicationHelper
+
+  def get_ia_generated_thumbnail(document, options={})
+    return unless document['canonicalUrl']
+
+    full_url = document['canonicalUrl']
+    return unless full_url =~ URI::regexp
+
+    site = nil
+    begin
+      site = URI.parse(full_url).host
+    rescue URI::InvalidURIError => e
+      return
+    end
+
+    image_tag("https://web.archive.org/thumb/#{site}?generate=1", options.merge(alt: document['meta_Title']))
+  end
+
 end
